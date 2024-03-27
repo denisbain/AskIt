39ee9de715b5076b2232620aa7d594ed4f1de620
Denis Bain <baindenis95@gmail.com>
2024-03-27 08:39:58 -0300

    Add sign up function wth bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
   resources :questions do
     resources :answers, except: [:new, :show]
   end
